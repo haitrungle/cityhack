6d4dd8d4d2d4cd269825ed85957598f081689867
Le Trung Hai <hozymary@gmail.com>
2022-12-23 15:30:41 +0800

    Add timeline
    - Timeline section
    - Extend Tailwind instead of using ad-hoc values
    - Remove redundant js file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        'primary': 'hsl(226deg 71% 40%)',
+        'secondary': 'hsl(142deg 77% 73%)',
+        'highlight': '#3fa5d4',
+        'selection-bg': 'rgb(64, 64, 64)',
+      },
+      padding: {
+        'fluid': 'max(calc((100vw - 72rem) / 2), 2rem)',
+      }
+    },
+  },
   plugins: [require("tailwindcss-fluid-type")],
 }
